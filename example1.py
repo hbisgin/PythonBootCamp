6badd1470637fd24f35f3fda42bfa2f05e224560
Halil Bisgin <halilbisgin@gmail.com>
2026-06-29 12:02:14 -0400

    first commit

---
+++ example1.py
@@ -0,0 +1,85 @@
+# This is a comment. When you try to compile your code, this line will be ignored. 
+
+x = 3 #variables are just names that point to values. In this case, x points to the value 3. And they help us store info.
+# Every variable will occupy some spae in your random access memory (RAM). The variable name is just a label for that space in memory.
+#512 vs 32 GB. Rams are usually in small size due to the fact that they are expensive. But they are very fast. The more RAM you have, the more data you can store in it and the faster your computer will be.
+y = 4
+z = x + y #assigning the sum of x and y to z. z will now point to the value 7.
+
+#variables can be of different types. For example, x and y are integers, but we can also have strings, floats (decimal numbers), booleans, etc.
+
+#integer example: 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1000000000
+#float example: 1.0, 2.5, 3.14, 4.0, 5.5 , real numbers, float
+#string example: "hello", "world", "python". Letters or characters they come together and form strings. 
+#strings are usually enclosed in quotes. You can use single quotes or double quotes.
+
+m = 3.14 #this is a float. It is a decimal number. It is a real number. It is a number that can have a decimal point.
+#m is a float variable. 
+
+e = "hello" #this is a string. It is a sequence of characters. It is a text. It is a string of characters. It is a string of letters. It is a string of words. It is a string of sentences. It is a string of paragraphs. It is a string of documents. It is a string of files. It is a string of data. It is a string of information. It is a string of knowledge. It is a string of wisdom. It is a string of understanding. It is a string of insight. It is a string of awareness. It is a string of consciousness. It is a string of existence.
+
+#x is an integer variable
+#y is an integer variable
+#z is an integer variable
+#m is a float variable
+#e is a string variable
+
+print(x, y, z, m, e)
+
+#I\O input output. You can write it, read it. 
+
+#If you ar assigning a value, that means you are writing.
+#If you are retriving, using, printing, that means you are reading.
+
+#if you have quote, that means you are dealing with a string. Otherwise, you are dealing with a number.
+
+f = "12.0" #this is a string, and you can't do any mathematical operations with it. You can only do string operations with it. 
+#You can concatenate it with other strings, but you can't add it to a number.
+
+g = "123948549850384898345904859089088"
+
+name = "Halil" 
+
+first_name = "Halil" #you can't have space in variable names.
+#all variables have to be one word!!!!!!!!!!!
+print(first_name)
+firstName = "Halil" #this is called camel case. The first letter of the first word is lowercase, and the first letter of the second word is uppercase. This is a common convention in programming.
+
+first_name = "John" #I can do this because I am reassigning  a new value to the variable first_name. The old value "Halil" is now lost, and first_name now points to the new value "John".
+print(first_name) #this will print "John" because that is the current value of first_name.
+
+#variables can store different values when you assign a new value. 
+
+#When you create variables, you need to follow certain rules:
+#1. You can't have spaces in variable names. You can use underscores instead of spaces.
+#2. You can't start variable names with numbers. You can use numbers in variable names, but not at the beginning.
+#3. You can't use special characters in variable names. You can only use letters, numbers
+
+#1number = 56 # you can't start variable names with numbers. This will cause a syntax error.
+
+#money$ = 100 # you can't use special characters in variable names. This will cause a syntax error.
+
+#hourly rate = 50 # you can't have spaces in variable names. This will cause a syntax error.
+
+last_name = "Smith" #this is a valid variable name. It follows all the rules.
+
+print(first_name, last_name) #this will print "John Smith" because that is the current value of first_name and last_name.
+
+weight = 30.5 #this is a float variable. It is a decimal number. It is a real number. It is a number that can have a decimal point.
+height =1.5 
+
+#can you divide your weight by your height? 
+#what are mathemtical operations in Python?
+#addition + 
+#subtraction -
+#multiplication *
+#division /
+#power is **, example: 2**3 = 8, 2 to the power of 3 is 8.
+
+# I sm not sayin 30.5/1.5 explicitly, but I am saying that you can divide your weight by your height.
+bmi = weight/height*height  #this formular is not currrently correct. 
+bmi = weight/height**2
+
+print(bmi)
+
+
